712801a723147ff4ffb742b637b5ab57ae94f4dd
Rincy Mathew <rmathew@tamu.edu>
2024-11-12 16:45:46 -0600

    unit test for prePersist() in EmbeddedVariableTest

--- components/src/test/java/org/folio/rest/workflow/model/EmbeddedVariableTest.java
+++ components/src/test/java/org/folio/rest/workflow/model/EmbeddedVariableTest.java
@@ -1,13 +1,22 @@
 package org.folio.rest.workflow.model;
 
+import static org.folio.spring.test.mock.MockMvcConstant.NULL_STR;
 import static org.folio.spring.test.mock.MockMvcConstant.VALUE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.util.ReflectionTestUtils.getField;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.folio.rest.workflow.enums.DirectoryAction;
 import org.folio.rest.workflow.enums.VariableType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class EmbeddedVariableTest {
 
@@ -91,6 +100,79 @@ class EmbeddedVariableTest {
 
     embeddedVariable.setAsTransient(true);
     assertEquals(true, getField(embeddedVariable, "asTransient"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("providePrePersistFor")
+  void prePersistWorksTest(Map<String, Object> initial, Map<String, Object> expected) {
+    initial.forEach((String attribute, Object value) -> {
+      setField(embeddedVariable, attribute, value);
+    });
+
+    embeddedVariable.prePersist();
+
+    expected.forEach((String attribute, Object value) -> {
+      assertEquals(value, getField(embeddedVariable, attribute));
+    });
+  }
+
+  /**
+   * Helper function for parameterized tests for the prePersist function.
+   *
+   * @return
+   *   The arguments array stream with the stream columns as:
+   *     - Arguments initial The initial values.
+   *     - Arguments expect The expected values.
+   */
+  private static Stream<Arguments> providePrePersistFor() {
+
+    return Stream.of(
+      Arguments.of(
+        helperFieldMap(null,  null,  null,  null),
+        helperFieldMap(false, false, false, VariableType.PROCESS)
+      ),
+      Arguments.of(
+        helperFieldMap(false, false, false, VariableType.PROCESS),
+        helperFieldMap(false, false, false, VariableType.PROCESS)
+      ),
+      Arguments.of(
+        helperFieldMap(true,  true,  true,  null),
+        helperFieldMap(true,  true,  true, VariableType.PROCESS)
+      ),
+      Arguments.of(
+        helperFieldMap(true,  true,  true,  VariableType.LOCAL),
+        helperFieldMap(true,  true, true,   VariableType.LOCAL)
+      ),
+      Arguments.of(
+        helperFieldMap(null,  null,  null,  VariableType.PROCESS),
+        helperFieldMap(false, false, false, VariableType.PROCESS)
+      ),
+      Arguments.of(
+        helperFieldMap(null,  true,  true,  null),
+        helperFieldMap(false, true,  true,  VariableType.PROCESS)
+      )
+    );
+  }
+
+  /**
+   * Helper for reducing inline code repititon for assignments.
+   *
+   * @param asJson The asJson value.
+   * @param asTransient The asTransient value.
+   * @param spin The spin value.
+   * @param type The type value.
+   *
+   * @return The built arguments map.
+   */
+  private static Map<String, Object> helperFieldMap(Boolean asJson, Boolean asTransient, Boolean spin, VariableType type ) {
+    final Map<String, Object> map = new HashMap<>();
+
+    map.put("asJson", asJson);
+    map.put("asTransient", asTransient);
+    map.put("spin", spin);
+    map.put("type", type);
+
+    return map;
   }
 
 }
